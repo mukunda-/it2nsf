9a9e906cbd65d4982212b0a1f8b3f03f519776bc
Roger <narayanroger@gmail.com>
2018-05-12 20:10:20 -0400

    Format python

--- LUT/sawVolumeMap.py
+++ LUT/sawVolumeMap.py
@@ -1,20 +1,20 @@
 # hello world of python
 # vrc6 sawtooth volume map (0..15 -> 0..42)
 
-output = open( "sawVolumeMap.txt", "w" )
-output.write( ";--------------------------------------------------------------------------------------------\n" )
-output.write( "@sawVolumeMap:\n" )
-output.write( ";--------------------------------------------------------------------------------------------\n" )
-output.write( "\t.byte " )
+output = open("sawVolumeMap.txt", "w")
+output.write(";--------------------------------------------------------------------------------------------\n")
+output.write("@sawVolumeMap:\n")
+output.write(";--------------------------------------------------------------------------------------------\n")
+output.write("\t.byte ")
 
-for i in range( 0, 16 ):
-    
-    value = int( round( i * 42.0 / 15.0 ) )
-    
-    output.write( hex( int(value) ).replace("0x","").upper().zfill(2) + "h" )
+for i in range(0, 16):
 
-    if( i != 15 ):
-        output.write( ", " )
+    value = int(round(i * 42.0 / 15.0))
 
-output.write( "\n" )
+    output.write(hex(int(value)).replace("0x", "").upper().zfill(2) + "h")
+
+    if (i != 15):
+        output.write(", ")
+
+output.write("\n")
 output.close()
